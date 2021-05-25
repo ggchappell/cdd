b0b4ec2b57650b3e1cfc1b48b08b473f2a7ea1f5
Glenn G. Chappell <ggchappell@alaska.edu>
2021-05-25 09:06:48 -0800

    cdd.bash: local var ARG -> __CDD_ARG

--- cdd.bash
+++ cdd.bash
@@ -7,16 +7,16 @@
 # Updated: 2021-05-25
 # https://github.com/ggchappell/cdd
 function cdd {
-    local ARG
+    local __CDD_ARG
 
     # Get the command-line argument, or, if there is none, do history
-    # substitution on !$. Store the result in variable ARG.
+    # substitution on !$. Store the result in variable __CDD_ARG.
     if (( $# >= 2 )); then
         echo >&2 "$FUNCNAME: Too many arguments"
         return 2
     elif (( $# == 1 )); then
-        ARG="$1"
-    else  # $# == 0: set ARG to history expansion of !$, if possible
+        __CDD_ARG="$1"
+    else  # $# == 0: set __CDD_ARG to history expansion of !$
         # Check if history enabled
         if [[ $(set +o) =~ \+o\ history ]]; then
             echo >&2 -n "$FUNCNAME: History expansion failed"
@@ -25,25 +25,25 @@ function cdd {
         fi
 
         # Do history expansion. Requires history to be enabled.
-        if ! ARG=$(history -p !$ 2> /dev/null); then
+        if ! __CDD_ARG=$(history -p !$ 2> /dev/null); then
             echo >&2 "$FUNCNAME: History expansion failed"
             return 1
         fi
 
-        # Evaluate ARG. If its value begins with '-', then avoid special
-        # treatment by evaluating with ' ' prepended. Otherwise. do not
-        # do this, as it prevents tilde expansion.
-        if [[ "${ARG:0:1}" == '-' ]]; then
-            ARG=$(eval "unset ARG; echo -E ' '$ARG")
-            ARG=${ARG:1}
+        # Evaluate $__CDD_ARG. If its value begins with '-', then avoid
+        # special treatment by evaluating with ' ' prepended. Otherwise,
+        # do not do this, as it prevents tilde expansion.
+        if [[ "${__CDD_ARG:0:1}" == '-' ]]; then
+            __CDD_ARG=$(eval "unset __CDD_ARG; echo -E ' '$__CDD_ARG")
+            __CDD_ARG=${__CDD_ARG:1}
         else
-            ARG=$(eval "unset ARG; echo -E $ARG")
+            __CDD_ARG=$(eval "unset __CDD_ARG; echo -E $__CDD_ARG")
         fi
     fi
 
-    # cd to $ARG, or, if this fails, to the result of "dirname $ARG".
-    if ! cd -- "$ARG" 2> /dev/null; then
-        local DIR=$(dirname -- "$ARG")
+    # cd to $__CDD_ARG, or, if this fails, to `dirname $__CDD_ARG`.
+    if ! cd -- "$__CDD_ARG" 2> /dev/null; then
+        local DIR=$(dirname -- "$__CDD_ARG")
         cd -- "$DIR"
         # For the above 'cd' command:
         # - Error messages go to our stderr.
